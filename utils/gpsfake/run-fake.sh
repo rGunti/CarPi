6df982a921aa5e247e7680085e393b639de7c5e8
Raphael "rGunti" Guntersweiler <raphael@rgunti.ch>
2017-11-26 14:05:46 +0100

    Adjusted run-fake to enable speed as parameter

--- utils/gpsfake/run-fake.sh
+++ utils/gpsfake/run-fake.sh
@@ -22,12 +22,13 @@
 # SOFTWARE.
 
 FILE_NAME=$1
+SPEED=$2
 
 echo "Shutting down GPSD before starting fakegps ..."
 sudo service gpsd stop
 
 echo "Starting gpsfake with $FILE_NAME (press Ctrl-C to stop) ..."
-gpsfake -c 0.1 $FILE_NAME
+gpsfake -c $SPEED $FILE_NAME
 
 echo "Restarting GPSD ..."
 sudo service gpsd start
