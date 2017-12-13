5572cacdb50ca63ae9026895effd09ff0b566c86
Raphael "rGunti" Guntersweiler <raphael@rgunti.ch>
2017-12-13 23:47:43 +0100

    Implemented an OBD service

--- CarPiDaemons/Obd2Daemon.py
+++ CarPiDaemons/Obd2Daemon.py
@@ -22,13 +22,150 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+from _socket import timeout
+from telnetlib import Telnet
+from time import sleep
 
-from obd import OBD, commands
+from os import path
+
+from CarPiConfig import init_config_env
+from CarPiLogging import EXIT_CODES, boot_print, end_print, print_unhandled_exception, log
+from CarPiThreading import CarPiThread
+from Obd2DataParser import transform_obj, parse_obj
+from RedisUtils import get_redis, set_piped
+from redis import exceptions as redis_exceptions
+
+
+APP_NAME = path.basename(__file__)
+
+
+class ObdTelnetPoller(CarPiThread):
+    def __init__(self, host='192.168.0.10', port=35000, init_sequence=[], poll_sequence=[]):
+        CarPiThread.__init__(self, 0.01)
+        self._data = None
+        self._t = Telnet()
+
+    def _do(self):
+        new_data = {}
+
+        self._data = new_data
+
+    def get_current_data(self):
+        return self._data
+
+
+class ObdEnvConfigError(Exception):
+    def __init__(self, sent_cmd, received_response):
+        self.sent_cmd = sent_cmd
+        self.received_response = received_response
+
+
+def send(t, cmd):
+    """
+    :param Telnet t:
+    :param str cmd:
+    :return str:
+    """
+    log('Sending {}'.format(cmd))
+    t.write('{}\r'.format(cmd))
+    data = ''
+    while '>' not in data:
+        data = data + t.read_eager()
+    log('{}: Received {} bytes'.format(cmd, len(data)))
+    return data.replace('\r', '|').strip()
+
+
+def get_arr(t, cmds):
+    """
+    :param Telnet t:
+    :param list of str cmds:
+    :return list of str:
+    """
+    log('Requesting {} items'.format(len(cmds)))
+    o = {}
+    for cmd in cmds:
+        o[cmd] = send(t, cmd)
+    return o
+
 
 if __name__ == "__main__":
-    connection = OBD("/dev/tty12")
-    cmd = commands.SPEED
+    EXIT_CODE = EXIT_CODES['OK']
 
-    response = connection.query(cmd)
+    CONFIG = init_config_env('CARPI_OBD_CONF', ['obd-daemon.conf', '/etc/carpi/obd-daemon.conf'])
+    boot_print(APP_NAME)
 
-    print response.value
+    CONFIG_OBD_HOST = CONFIG.get('PollerSource', 'host')
+    CONFIG_OBD_PORT = CONFIG.getint('PollerSource', 'port')
+    CONFIG_OBD_TIMEOUT = CONFIG.getint('PollerSource', 'timeout')
+    CONFIG_OBD_RETRY_TIMEOUT = CONFIG.getint('PollerSource', 'retry_timeout')
+
+    CONFIG_INIT_SEQ = CONFIG.get('DataPoller', 'init_sequence').split(',')
+    CONFIG_POLL_SEQ = CONFIG.get('DataPoller', 'poll_sequence').split(',')
+
+    log("Initialize Redis Connection ...")
+    R = get_redis(CONFIG)
+
+    T = None  # type: Telnet
+
+    try:
+        log("OBD Daemons is running ...")
+        while True:
+            T = Telnet()
+
+            try:
+                log("Connecting to OBD Adapter ...")
+                T.open(host=CONFIG_OBD_HOST,
+                       port=CONFIG_OBD_PORT,
+                       timeout=CONFIG_OBD_TIMEOUT)
+
+                log("Awaiting response ...")
+                T.write('\r')
+                T.read_until('>', timeout=CONFIG_OBD_TIMEOUT)
+
+                log("Configuring Environment ...")
+                for cmd in CONFIG_INIT_SEQ:
+                    r = send(T, cmd)
+                    if 'ERROR' in r:
+                        raise ObdEnvConfigError(cmd, r)
+
+                log("Configuration completed, starting recording loop ...")
+                while True:
+                    data = transform_obj(parse_obj(get_arr(T, CONFIG_POLL_SEQ)))
+                    set_piped(R, data)
+            except timeout:
+                log("Connection to OBD Adapter timed out after {} sec, retrying after {} sec".format(
+                    CONFIG_OBD_TIMEOUT, CONFIG_OBD_RETRY_TIMEOUT))
+                sleep(CONFIG_OBD_RETRY_TIMEOUT)
+            except ObdEnvConfigError as e:
+                log("Failed to configure environment (Sent={}, Received={})".format(e.sent_cmd,
+                                                                                    e.received_response))
+                sleep(CONFIG_OBD_RETRY_TIMEOUT)
+            finally:
+                try:
+                    log("Trying to close Telnet connection ...")
+                    T.close()
+                except (KeyboardInterrupt, SystemExit, redis_exceptions.ConnectionError) as e:
+                    # Rethrow the important ones for good measure
+                    raise e
+                except:  # Just catch everything else when closing
+                    pass
+                finally:
+                    T = None
+    except (KeyboardInterrupt, SystemExit):
+        log("Shutdown requested!")
+    except redis_exceptions.ConnectionError:
+        EXIT_CODE = EXIT_CODES['DataDestinationLost']
+        log("Connection to Redis Server lost! Daemon is quitting and waiting for relaunch")
+    except:
+        EXIT_CODE = EXIT_CODES['UnhandledException']
+        print_unhandled_exception()
+    finally:
+        if T:
+            log("Trying to close Telnet connection ...")
+            try:
+                T.close()
+            except:
+                pass
+
+    end_print()
+    exit(EXIT_CODE)
---
+++ CarPiDaemons/Obd2DataParser.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python
+"""
+MIT License
+
+Copyright (c) 2017 Raphael "rGunti" Guntersweiler
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from CarPiLogging import log
+from RedisKeys import ObdRedisKeys
+
+
+class ObdPidParserUnknownError(Exception):
+    def __init__(self, type):
+        log("Unknown or unimplemented OBD PID {}".format(type))
+        self.type = type
+
+
+def trim_obd_value(v):
+    """
+    Trims unneeded data from an OBD response
+    :param str v:
+    :return str:
+    """
+    if len(v) > 4:
+        return ''
+    else:
+        return v[4:]
+
+
+def parse_value(type, val):
+    """
+    Parses a given OBD value of a given type (PID)
+    and returns the parsed value.
+    If the PID is unknown / not implemented a PIDParserUnknownError
+    will be raised including the type which was unknown
+    :param type:
+    :param val:
+    :return:
+    """
+    if type in PARSER_MAP:
+        return PARSER_MAP[type](val)
+    else:
+        raise ObdPidParserUnknownError(type)
+
+
+def parse_obj(o):
+    """
+    Parses a given dictionary with the key being the OBD PID and the value its
+    returned value by the OBD interface
+    :param dict o:
+    :return:
+    """
+    r = {}
+    for k, v in o.items():
+        r[k] = parse_value(k, v)
+    return o
+
+
+def transform_obj(o):
+    r = {}
+    for k, v in o:
+        if v is tuple:
+            keys = OBD_REDIS_MAP[k]
+            r[keys[0]] = v[0]
+            r[keys[1]] = v[1]
+        else:
+            r[OBD_REDIS_MAP[k]] = v
+
+
+def parse_atrv(v):
+    """
+    Parses the battery voltage and returns it in [Volt] as float with 1 decimal place
+    :param str v: e.g. "12.3V"
+    :return float:
+    """
+    try:
+        return float(v.replace('V', ''))
+    except ValueError:
+        return float('nan')
+
+
+def parse_0104(v):
+    """
+    Parses the calculated engine load and returns it as an integer from 0 - 100
+    :param str v: e.g. "410405"
+    :return int: e.g. 2
+    """
+    try:
+        val = int(trim_obd_value(v), 16)
+        return val / 2.55
+    except ValueError:
+        return int('nan')
+
+
+def parse_010B(v):
+    """
+    Parses Intake MAP and returns it in [kPa] as an integer from 0 - 255
+    :param str v:
+    :return int:
+    """
+    try:
+        return int(trim_obd_value(v), 16)
+    except ValueError:
+        return int('nan')
+
+
+def parse_010C(v):
+    """
+    Parses Engine RPM and returns it in [RPM] as a float from 0 - 16383.75
+    :param str v:
+    :return float:
+    """
+    try:
+        val = int(trim_obd_value(v), 16)
+        return val / 4
+    except ValueError:
+        return float('nan')
+
+
+def parse_010D(v):
+    """
+    Parses Vehicle Speed and returns it in [km/h] as an integer from 0 - 255
+    :param str v:
+    :return int:
+    """
+    try:
+        return int(trim_obd_value(v), 16)
+    except ValueError:
+        return float('nan')
+
+
+def parse_010F(v):
+    """
+    Parses Intake Air Temperature and returns it in [degrees C] as an integer from -40 - 215
+    :param str v:
+    :return int:
+    """
+    try:
+        val = int(trim_obd_value(v), 16)
+        return val - 40
+    except ValueError:
+        return float('nan')
+
+
+def parse_0134_013B(v):
+    """
+    Parses the O2 Sensor Value (0134 - 013B) and returns two values parsed from it:
+    1. Fuel-Air Equivalence [Ratio] as a float from 0 - 2
+    2. Current in [mA] as a float from -128 - 128
+    :param str v:
+    :return tuple of float, float:
+    """
+    try:
+        trim_val = trim_obd_value(v)
+        val_ab = int(trim_val[0:2], 16)
+        val_cd = int(trim_val[2:4], 16)
+        return (2 / 65536) * val_ab, val_cd - 128
+    except ValueError:
+        return float('nan'), float('nan')
+
+
+PARSER_MAP = {
+    'ATRV': parse_atrv,
+    '0104': parse_0104,
+    '010B': parse_010B,
+    '010C': parse_010C,
+    '010D': parse_010D,
+    '010F': parse_010F,
+    '0134': parse_0134_013B,
+    '0135': parse_0134_013B,
+    '0136': parse_0134_013B,
+    '0137': parse_0134_013B,
+    '0138': parse_0134_013B,
+    '0139': parse_0134_013B,
+    '013A': parse_0134_013B,
+    '013B': parse_0134_013B
+}
+
+OBD_REDIS_MAP = {
+    'ATRV': ObdRedisKeys.KEY_BATTERY_VOLTAGE,
+    '0104': ObdRedisKeys.KEY_ENGINE_LOAD,
+    '010B': ObdRedisKeys.KEY_INTAKE_MAP,
+    '010C': ObdRedisKeys.KEY_ENGINE_RPM,
+    '010D': ObdRedisKeys.KEY_VEHICLE_SPEED,
+    '010F': ObdRedisKeys.KEY_INTAKE_TEMP,
+    '0134': (ObdRedisKeys.KEY_O2_SENSOR_FAEQV, ObdRedisKeys.KEY_O2_SENSOR_CURRENT),
+    '0135': (ObdRedisKeys.KEY_O2_SENSOR_FAEQV, ObdRedisKeys.KEY_O2_SENSOR_CURRENT),
+    '0136': (ObdRedisKeys.KEY_O2_SENSOR_FAEQV, ObdRedisKeys.KEY_O2_SENSOR_CURRENT),
+    '0137': (ObdRedisKeys.KEY_O2_SENSOR_FAEQV, ObdRedisKeys.KEY_O2_SENSOR_CURRENT),
+    '0138': (ObdRedisKeys.KEY_O2_SENSOR_FAEQV, ObdRedisKeys.KEY_O2_SENSOR_CURRENT),
+    '0139': (ObdRedisKeys.KEY_O2_SENSOR_FAEQV, ObdRedisKeys.KEY_O2_SENSOR_CURRENT),
+    '013A': (ObdRedisKeys.KEY_O2_SENSOR_FAEQV, ObdRedisKeys.KEY_O2_SENSOR_CURRENT),
+    '013B': (ObdRedisKeys.KEY_O2_SENSOR_FAEQV, ObdRedisKeys.KEY_O2_SENSOR_CURRENT)
+}
+
+if __name__ == "__main__":
+    print("This script is not intended to be run standalone!")
